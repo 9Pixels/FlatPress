cd4cef5f65863d1c0dfdeb4f9f44c4b8cbec3303
Hapiuc Robert <robert@9pixels.co>
2015-05-17 19:38:11 +0300

    color change and improvments

--- functions.php
+++ functions.php
@@ -3,18 +3,20 @@
 /**
  *	Theme Setup
  */
-if( !function_exists( 'flatpresstheme_setup' ) ) {
-	add_action( 'after_setup_theme', 'flatpresstheme_setup' );
-	function flatpresstheme_setup() {
+if( !function_exists( 'materialx_theme_setup' ) ) {
+	add_action( 'after_setup_theme', 'materialx_theme_setup' );
+	function materialx_theme_setup() {
 		load_theme_textdomain( 'accountant', get_template_directory() . '/languages' );
 	}
 }
 /**
  *	Enqueue Styles
  */
-if( !function_exists( 'flatpressenqueue_styles' ) ) {
-	add_action( 'wp_enqueue_scripts', 'flatpressenqueue_styles' );
-	function flatpressenqueue_styles() {
+if( !function_exists( 'materialx_enqueue_styles' ) ) {
+	add_action( 'wp_enqueue_scripts', 'materialx_enqueue_styles' );
+	function materialx_enqueue_styles() {
+		wp_enqueue_style( 'font-family-roboto', 'http://fonts.googleapis.com/css?family=Roboto:200,300,400,700,600,400italic,700italic,300italic');
+		wp_enqueue_style( 'font-family-roboto-slab', 'http://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' );
 	    wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css' );
 	}
 }
