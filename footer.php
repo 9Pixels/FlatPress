75b09750eaf9fbcddfe65de7453511cee4420646
Robciuc Ioan <ionut@9pixels.co>
2015-06-11 17:17:49 +0300

    Issue (front-page.php theme text-domain) fixed
    Issue (front-page.php theme text-domain) fixed.

--- footer.php
+++ footer.php
@@ -9,7 +9,7 @@
 				<?php flat_hook_footer_before(); ?>
 				<footer class="site-info" itemscope itemtype="http://schema.org/WPFooter">
 					<?php flat_hook_footer_top(); ?>
-					<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'flat' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'flat' ); ?>"><?php printf( __( 'Proudly powered by %s', 'flat' ), 'WordPress' ); ?></a>.
+					<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'flatpress' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'flatpress' ); ?>"><?php printf( __( 'Proudly powered by %s', 'flatpress' ), 'WordPress' ); ?></a>.
 				</footer>
 				<?php flat_hook_footer_after(); ?>
 			<?php endif; ?>
